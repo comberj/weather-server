03b0ba942a92e2216b2c1bb85470caefba1d45a9
Jon Comberbach <jonathan.comberbach@gmail.com>
2020-10-24 20:54:15 -0700

    feat: add keys

--- src/modules/weather.service.ts
+++ src/modules/weather.service.ts
@@ -3,8 +3,9 @@ import 'dotenv/config'
 
 export class WeatherService {
     private constructUrl = (api:string, city:string):string => {
-        const apiKey = process.env.API_KEY
-        return `http://api.openweathermap.org/data/2.5/${api}?q=${city}&appid=${apiKey}`
+        const apiKey = process.env.WEATHER_KEY
+
+        return `http://api.openweathermap.org/data/2.5/${api}?q=${city}&appid=${apiKey}&units=metric`
     }
 
     private makeRequest = async (url:string) => {
@@ -12,6 +13,11 @@ export class WeatherService {
             .then( res => res.json() )
             .then( body =>  {
             return {
+                statusCode: 200,
+                headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Credentials': true,
+                },
                 body: JSON.stringify({ body })
             };
         });
